da48a9f3f0f1f674e308611410d15766c37230cf
rwson <rw_Song@sina.com>
2017-07-19 15:27:02 +0800

    plain to add nodejs support

---
+++ node/index.js
@@ -0,0 +1,11 @@
+import pathToRegExp from "path-to-regexp";
+var HttpError = require("http-errors");
+var methods = require("methods");
+
+export default class MiniRouter {
+	constructor(opts = {}) {
+		this.opts = opts;
+	}
+
+	
+}
